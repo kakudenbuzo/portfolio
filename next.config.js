8f3bfd755bbf8406b91249dedc33bba8d67559eb
kakudenbuzo <kakudenbuzou@gmail.com>
2023-05-28 18:43:12 +0200

    cd: Add basePath

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   output: "export",
+  assetPrefix: isProd ? "/portfolio/" : "",
+  basePath: isProd ? "/portfolio" : "",
+  images: {
+    unoptimized: true,
+  },
 };
-
 module.exports = nextConfig;
